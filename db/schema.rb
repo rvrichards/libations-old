4378a838fd184400ca58f323d7e8f7eacebe80fd
Rob Richards <rob@rtfm.ca>
2015-12-26 18:20:43 +0100

    Added first, last and username. Edited views.

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,9 @@ ActiveRecord::Schema.define(version: 20151226165249) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
+    t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
