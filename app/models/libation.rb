7f006bc1a5f6ef8f1297446c30ec78425cf00652
Rob Richards <rob@rtfm.ca>
2016-02-13 09:50:42 +0100

    Working seed file. Yay!

--- app/models/libation.rb
+++ app/models/libation.rb
@@ -4,5 +4,5 @@ class Libation < ActiveRecord::Base
 
   enum container_type: { Aluminium: 1, Glass: 2,Plastic: 3, Steel: 4, Tetra: 5, Cardboard: 6 ,Other: 7}
   enum measurement_type: { grams: 1, litres: 2, millilitres: 3, ounces: 4, unknown: 99}
-  enum libation_type: { drink: 1,  juice: 2, soda: 3, tea: 4, dairy: 5, wine: 6, beer: 7, other:99 }
+  enum libation_type: { drink: 1,  juice: 2, soda: 3, tea: 4, dairy: 5, wine: 6, beer: 7, cooler: 8, spirits: 9, liqueurs:10, other:99 }
 end
